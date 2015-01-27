ee1e1bb9f36fbe961daa600428371462428f8d7e
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-01-26 22:14:44 -0800

    Preliminary ipfs readonly Android app

---
+++ ipfs/src/main/java/org/ipfs/android/MainActivity.java
@@ -0,0 +1,62 @@
+package org.ipfs.android;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        Log.d("ipfs", "New intent! ");
+        if (uri != null && uri.getScheme().equals("ipfs")) {
+            String fileHash = uri.getPath();
+            Log.d("ipfs", fileHash);
+            ((TextView) findViewById(R.id.hash)).setText(fileHash);
+
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_VIEW);
+            String url = "http://gateway.ipfs.io/ipfs/" + fileHash;
+            sendIntent.setData(Uri.parse(url));
+            startActivity(sendIntent);
+        }
+    }
+}
